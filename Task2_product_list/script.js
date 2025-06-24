c22ff6424d2834b43bf476ecab59f4b8db9e70c4
Tarun <tarungupta9917@gmail.com>
2025-06-24 09:12:15 +0530

    Task 2

--- Task2_product_list/script.js
+++ Task2_product_list/script.js
@@ -1,40 +1,54 @@
 const btn = document.getElementById('btn');
 const inp = document.getElementById('inp');
-const tempUrl = "https://dummyjson.com/products/search?q="
+const sortSelect = document.getElementById('sort');
+const tempUrl = "https://dummyjson.com/products/search?q=";
 const container = document.getElementById('container');
 
-btn.addEventListener('click',()=>{
-    const URL = tempUrl+inp.value;
+btn.addEventListener('click', () => {
+    const URL = tempUrl + inp.value;
+    container.innerHTML = ""; 
 
-    if(inp.value!=""){
+    if (inp.value !== "") {
         fetch(URL)
-        .then((res)=>{
-            return res.json()
-        })
-        .then((data)=>{
-            // console.log(data.products);
-            for(let product of data.products){
-                console.log(product);
-                const productElem = document.createElement('div');
+            .then((res) => res.json())
+            .then((data) => {
+                let products = data.products;
 
-                const h2 = document.createElement('h2');
-                h2.innerText = product.title;
                 
-                const img = document.createElement('img');
-                img.setAttribute('src',product.images[0])
-                img.style.width = "200px"
+                const sortValue = sortSelect.value;
+                if (sortValue === "asc") {
+                    products.sort((a, b) => a.price - b.price);
+                } else if (sortValue === "desc") {
+                    products.sort((a, b) => b.price - a.price);
+                }
 
-                const para = document.createElement('p');
-                para.innerText = product.price;
+                
+                for (let product of products) {
+                    const productElem = document.createElement('div');
 
-                productElem.append(h2);
-                productElem.append(img);
-                productElem.append(para);
-                productElem.style.border= "2px solid black"
+                    const h2 = document.createElement('h2');
+                    h2.innerText = product.title;
 
-                container.append(productElem);
-            }
-            
-        })
+                    const img = document.createElement('img');
+                    img.setAttribute('src', product.images[0]);
+                    img.style.width = "200px";
+
+                    const para = document.createElement('p');
+                    para.innerText = product.price;
+
+                    productElem.append(h2, img, para);
+                    productElem.style.border = "2px solid black";
+                    productElem.style.padding = "10px";
+                    productElem.style.margin = "10px";
+
+                    container.append(productElem);
+                }
+            })
+            .catch(err => {
+                console.error("Fetch error:", err);
+                container.innerHTML = "<p>Something went wrong. Please try again.</p>";
+            });
+    } else {
+        container.innerHTML = "<p>Please enter a search term.</p>";
     }
-})
+});
